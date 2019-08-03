1f8df2e391d0bd3d8cc29a7c9e2c334f67825dd6
Ben Hoyt <nonmom64@gmail.com>
2019-08-03 18:12:17 -0500

    migrated to android x

--- app/src/main/java/com/example/benji/benstimer/MainActivity.kt
+++ app/src/main/java/com/example/benji/benstimer/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.benji.benstimer
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 import android.view.MenuItem
 
